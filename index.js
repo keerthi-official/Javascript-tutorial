8dfb32942d69cd785d7afec772871f0bd57bd00f
keerthi-official <keerthimin1811@gmail.com>
2026-05-20 21:37:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//console.log("Hello World!");
+
+//console.log(myCityName);
+//var myCityName = "New York City";
+///myCityName = "Pondy";
+
+// EX: 01. template literals and prompt / alert
+const myName = prompt("What is your name?");
+const myBirthDate = prompt("What is your birth date?");
+const myBirthMonth = prompt("What is your birth month?");
+const myBirthYear = prompt("What is your birth year?");
+const myCityName = prompt("What city do you live in?");
+console.log(`${myName} is learning javascript from the scratch.`);
+console.log(`${myBirthDate} of ${myBirthMonth},${myBirthYear} is my birthday.`);
+console.log(`${myCityName} is my city.`);
