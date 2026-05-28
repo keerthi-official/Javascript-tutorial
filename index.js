26718f4cdfa9943809d53ebdc44e8e9db304f79f
keerthi-official <keerthimin1811@gmail.com>
2026-05-28 21:01:57 +0530

    Update project

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ if (age > 17) {
 //Obesity: BMI 30.0 or greater.
 //Give me program ang get the input form user
 
-const weight = prompt("Enter your weight:");
+/*const weight = prompt("Enter your weight:");
 const height = prompt("Enter your height:");
 
 // BMI Formula
@@ -82,7 +82,91 @@ if (bmi < 18.5) {
 } else if (bmi >= 18.5 && bmi <= 24.9) {
   console.log("Your Category is Healthy Weight");
 } else if (bmi >= 25 && bmi <= 29.9) {
-  console.log("Your Category is Overweight");
-} else {
-  console.log("Your Category is Obesity");
+//  console.log("Your Category is Overweight");
+//} else {
+//  console.log("Your Category is Obesity");
+//} */
+
+// Regular Function
+//function greetUser(nameOfPerson) {
+//  return `Hello, ${nameOfPerson}`;
+//}
+
+// function greetUser(nameOfPerson) {
+//   console.log(arguments);
+//   return `Hello, ${nameOfPerson}`;
+// }
+
+// const greetUser = (nameOfPerson) => `Hello, ${nameOfPerson}`;
+
+// Function Call
+// greetUser("keerthi");
+// greetUser("kiki");
+
+//console.log(greetUser("Hilary"));
+// console.log(greetUser("Hilary"));
+// console.log("Hello Everyone");
+
+// function myAgeCalc(birth) {
+//   return 2026 - birth;
+// }
+
+// Function Expression or Expression Function
+// let myAgeCalc = function (birth) {
+//   return 2026 - birth;
+// };
+
+// Arrow Function
+// const myAgeCalc = (birth) => 2026 - birth;
+// console.log(myAgeCalc(2000));
+
+// IIFE Immeditely Invoked Functions
+// const age = ((birth) => 2026 - birth)(2000);
+// console.log(age);
+
+// console.dir(greetUser("keerthi"));
+
+// Rest and Spread
+// const [x, y, arr] = [123, 345, ["keerthi", "Pondicherry"]];
+// const result = [y, ...arr];
+// console.log(result);
+
+// const exampleArrFn = (...args) => {
+//   console.log(args);
+// };
+
+// exampleArrFn(100, 300, 400);
+// exampleArrFn(100);
+
+// EX: 04 We have to build a calculator for Finding the winner in a Volleyball Tournament
+//  by giving their series of matches score as a input to a function named `winnerOfTournament`.
+// There 3 different teams have played a series of matches in the tournament with one vs another.
+//  Scores are given below
+// TEAM 1 : 24 40 10
+// TEAM 2 : 30 10 44 12
+// TEAM 3 : 50 40 20 09
+
+/*function average(team1, team2, team3) {
+  let avg1 = (24 + 40 + 10) / 3;
+  let avg2 = (30 + 10 + 44) / 3;
+  let avg3 = (50 + 40 + 20) / 3;
+
+  console.log(avg1);
+  console.log(avg2);
+  console.log(avg3);
+
+  if (avg1 > avg2 && avg1 > avg3) {
+    console.log("Team 1 is Winner");
+  } else if (avg2 > avg1 && avg2 > avg3) {
+    console.log("Team 2 is Winner");
+  } else {
+    console.log("Team 3 is Winner");
+  }
 }
+
+average(); */
+
+//LOOPING STATEMENTS
+
+//WHILE LOOP
+//let
